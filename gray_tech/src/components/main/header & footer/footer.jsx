cdf2c66d086e8f84e2a592c9dff250aee91a96f9
Palanga123 <palangageorgen@gmail.com>
2024-09-10 18:02:31 +0200

    done the footer

--- gray_tech/src/components/main/header & footer/footer.jsx
+++ gray_tech/src/components/main/header & footer/footer.jsx
@@ -1,31 +1,47 @@
 import { FaEnvelope, FaFacebook, FaInstagram, FaPhone, FaTwitter } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
 
-    return(
+    return (
         <>
-            <footer className="bottom-0  p-4 md:p-8 bg-gray-300 text-black w-full">
-                <div className="w-11/12 md:w-4/5 m-auto grid md:grid-cols-2 gap-3">
+            <footer className="bottom-0 text-black w-full mt-auto">                
+                <div className="w-11/12 md:w-4/5 m-auto">
+                    {/* <hr className="border-gray-400"/> */}
+                    <div className="w-full items-center flex justify-between py-6">
                         <div className="w-full md:w-1/2">
-                            <p>You do not need to worry about clearing your cargo. <br/> We are reliable.</p><br/><br/>
-                            <p><i>Zamola. Here for you</i></p><br/>
-                            
-                            
-                            
-                        </div>
-                        <div className="w-full md:w-1/2">
-                            <p className="text-lg md:text-2xl font-semibold mb-2">Contact Us</p>
-                            <p className="flex items-center"><FaPhone className="text-[18px] mr-3"/> <a href="tel:0765735764">+260 734 123 123</a></p>
-                            <p  className="flex items-center mt-6"><FaEnvelope className="text-[18px] mr-3"/> <a href="mailto:geocademyedu@gmail.com">zamola@gmail.com</a></p><br/>
-                            <p className="font-bold">Socials:</p>
-                            <div class="flex h-10 mb-2 text-center w-full">
-                                <button className="p-4 text-[26px] rounded-md text-center" onClick = "location.href='https://www.facebook.com/profile.php?id=100092004857675'"><FaFacebook/></button> 
-                                <button className="p-4 text-[26px] rounded-md text-center" onClick = "location.href='https://twitter.com/Geocademy_'"><FaTwitter/></button> 
-                                <button className="p-4 text-[26px] rounded-md text-center" onClick = "location.href='https://instagram.com/geocademyedu'"><FaInstagram/></button> 
+
+                            <div>
+                                <NavLink to="/">
+                                    <div className="">
+                                        <p className="font-semibold text-[14px] sm:text-lg text-[#1352cf]">Zamola <span className="text-gray-600">Logistics Ltd.</span></p>
+                                        <p className="text-[8px] md:text-[10px]">Clearing & Forwarding</p>
+                                    </div>
+                                </NavLink>
+                            </div>
+
+                            <div class="flex h-10 mb-2 text-center w-full mt-10">
+                                <button className="h-10 w-10 border rounded-full mr-2 bg-gray-300" onClick="location.href='https://www.facebook.com/profile.php?id=100092004857675'"><FaFacebook className="m-auto" /></button>
+                                <button className="h-10 w-10 border rounded-full mx-2 bg-gray-300" onClick="location.href='https://twitter.com/Geocademy_'"><FaTwitter className="m-auto" /></button>
+                                <button className="h-10 w-10 border rounded-full mx-2 bg-gray-300" onClick="location.href='https://instagram.com/geocademyedu'"><FaInstagram className="m-auto" /></button>
+                                <button className="h-10 w-10 border rounded-full mx-2 bg-gray-300" onClick="location.href='https://instagram.com/geocademyedu'"><FaEnvelope className="m-auto" /></button>
                             </div>
                         </div>
-                    </div><br/><br/>
-                    <p className="text-center font-semibold">Zamola Logistics Limitted <br/>&copy; 2024 All Rights Reserved</p>
+                        <div className="w-full md:w-1/2 text-sm flex">
+                            <div className="w-2/3"></div>
+                                <div className="w-1/3">
+                                <p className="font-bold text-[16px] py-2">Useful links</p>
+                                <NavLink to="/"><p className="py-2">Home</p></NavLink>
+                                <NavLink to="/services"><p className="py-2">Services</p></NavLink>
+                                <NavLink to="/about"><p className="py-2">About</p></NavLink>
+                                <NavLink to="/contact"><p className="py-2">Contact Us</p></NavLink>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    {/* <hr className="border-gray-400"/> */}
+                </div>
+                <p className="text-center font-semibold text-sm text-white py-4 bg-[#1352cf]">&copy; {new Date().getFullYear()} All Rights Reserved</p>
             </footer>
         </>
     );
