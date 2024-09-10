0e2760aa95ddab949cbc071231127775ac1ba204
Palanga123 <palangageorgen@gmail.com>
2024-09-10 19:29:38 +0200

    items-center

--- gray_tech/src/components/main/about/culture.jsx
+++ gray_tech/src/components/main/about/culture.jsx
@@ -1,7 +1,7 @@
 const Culture = (props) => {
     return (
         <>
-            <div className='flex flex-row md:flex-column rounded-md p-2 md:p-4 text-sm justify-between'>
+            <div className='flex flex-row md:flex-column rounded-md p-2 md:p-4 text-sm justify-between md:items-center'>
                 <div>
                     <button className='bg-gray-200 rounded p-4 text-blue-900 mr-3 md:mb-3'>
                         {props.icon}
