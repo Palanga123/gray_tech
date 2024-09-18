9d19b66fdfbb31aeac550cf73f26d2b351172add
Palanga123 <palangageorgen@gmail.com>
2024-09-19 01:43:16 +0200

    added whatsapp number

--- gray_tech/src/components/main/header & footer/footer.jsx
+++ gray_tech/src/components/main/header & footer/footer.jsx
@@ -7,9 +7,11 @@ const Footer = () => {
         <>
             <div className="w-11/12 md:w-4/5 mx-auto">
                 <div className="h-10 md:h-14 fixed right-4 bottom-6 w-full flex flex-row-reverse">
-                    <div className="w-10 md:w-14 h-10 md:h-14 rounded-full bg-green-500 hover:bg-green-600 hover:w-44 duration-700">
-                        <button className="h-full w-full text-white bg-transparent px-3 md:px-[18px] rounded-full md:text-xl"><FaWhatsapp/></button>
-                    </div>
+                    <a href="https://wa.me/+260765735764">
+                        <div className="w-10 md:w-14 h-10 md:h-14 rounded-full bg-green-500 hover:bg-green-600 duration-500" onClick={Location.href="https://facebook.com"}>
+                            <button className="h-full w-full text-white bg-transparent px-3 md:px-[18px] rounded-full md:text-xl"><FaWhatsapp/></button>
+                        </div>
+                   </a>
                 </div>  
             </div>
             
