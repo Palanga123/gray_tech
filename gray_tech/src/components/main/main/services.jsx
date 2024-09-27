321c58d7f2b8f43e29c9c38f45f3029b758f49ed
Palanga123 <palangageorgen@gmail.com>
2024-09-27 06:55:05 +0200

    services page

--- gray_tech/src/components/main/main/services.jsx
+++ gray_tech/src/components/main/main/services.jsx
@@ -1,28 +1,37 @@
 import Typing from '../../images/typing.jpg';
 
 const Services = () => {
-    
+
     return (
         <>
-            <div className="md:h-64 w-full bg-gray-50 border border-gray-200 rounded md:flex items-center md:justify-between overflow-hidden">
-                <div className="w-full md:w-[40%] h-full">
-                    <img
-                        src={Typing}
-                        className="w-full md:h-full md:rounded-l"
-                        alt="woman typing"
-                    />
-                </div>
-                <div className="h-full w-full md:w-3/5">
+            <div className='h-36'></div>
+            <p className='font-bold text-[#1352cf] text-3xl my-10 text-center'>Our Services</p>
+            
+            
+            <div className='w-11/12 md:w-4/5 m-auto sm:grid grid-cols-2 gap-5'>
+                <div className="my-5 w-full bg-gray-50 border border-gray-200 rounded overflow-hidden p-8">
 
-                    <div className="px-8">
-                        <p className="text-[16px] md:text-[20px] font-bold py-3 text-[#1352cf]">Do Business with us and enjoy personalized clearance rates.</p>
-                        <p className="text-sm md:text-base">Benefit from our services and solutions designed to meet all of your shipping needs.</p>
+                    <p className="text-[16px] md:text-[20px] font-bold py-2 text-[#1352cf]">Cargo Clearance.</p>
+                    <div className="w-full py-4">
+                        <p className="text-sm md:text-base">Benefit from our clearance services which are speedy and effecient. Don't worry about your cargo, leave it to us.</p>
 
-                        <div className="my-3">
+                        <div className="mt-8">
                             <button className="text-[14px] px-4 py-3 text-white rounded bg-[#1352cf] font-semibold hover:scale-105 hover:duration-500 duration-500">Reach Out</button>
                         </div>
                     </div>
                 </div>
+                <div className="my-5 w-full bg-gray-50 border border-gray-200 rounded overflow-hidden p-8">
+
+                    <p className="text-[16px] md:text-[20px] font-bold py-2 text-[#1352cf]">Cargo Clearance.</p>
+                    <div className="w-full py-4">
+                        <p className="text-sm md:text-base">Benefit from our clearance services which are speedy and effecient. Don't worry about your cargo, leave it to us.</p>
+
+                        <div className="mt-8">
+                            <button className="text-[14px] px-4 py-3 text-white rounded bg-[#1352cf] font-semibold hover:scale-105 hover:duration-500 duration-500">Reach Out</button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     );
